2bc78ec47a0087cdac00d8003b6d5988ae4fd5ff
CodeSandbox <support@codesandbox.io>
2025-06-26 14:18:35 +0000

    Sandbox snapshot commit

--- src/layout/TopBar/index.tsx
+++ src/layout/TopBar/index.tsx
@@ -1,7 +1,12 @@
 import styles from "./styles.module.css";
 
 function TopBar() {
-  return <div className={styles.container}>Top Bar</div>;
+  return (
+    <>
+      <div className={styles.container}>Top Bar</div>
+      <div className={`${styles.container} ${styles.container_fake}`}></div>
+    </>
+  );
 }
 
 export default TopBar;
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./SideBar";
 import TopBar from "./TopBar";
+import styles from "./styles.module.css";
 
 function LayOut() {
   return (
     <>
-      <TopBar />
-      <SideBar />
-      <Outlet />
+      <header>
+        <TopBar />
+      </header>
+      <nav>
+        <SideBar />
+      </nav>
+      <main className={styles.main_container}>
+        <Outlet />
+      </main>
     </>
   );
 }
