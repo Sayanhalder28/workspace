4b886c0de150a543adb7975c186456c909623985
CodeSandbox <support@codesandbox.io>
2025-06-30 10:16:28 +0000

    Sandbox snapshot commit

--- src/layout/TopBar/index.tsx
+++ src/layout/TopBar/index.tsx
@@ -1,12 +1,7 @@
 import styles from "./styles.module.css";
 
 function TopBar() {
-  return (
-    <header>
-      <div className={styles.container}>Top Bar</div>
-      <div className={`${styles.container} ${styles.container_fake}`}></div>
-    </header>
-  );
+  return <header></header>;
 }
 
 export default TopBar;
