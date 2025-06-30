61e09d030615d9244cc5d247aac65659a527a2c8
CodeSandbox <support@codesandbox.io>
2025-06-30 11:52:03 +0000

    Sandbox snapshot commit

--- src/layout/TopBar/index.tsx
+++ src/layout/TopBar/index.tsx
@@ -1,10 +1,19 @@
 import styles from "./styles.module.css";
+import BrandLogo from "../../assets/J&JLogo.svg";
 
 function TopBar() {
   return (
     <header>
-      <div className={`${styles.container} ${styles.container_items}`}>
-        <div className={styles.section_left}>left half</div>
+      <div className={`${styles.container} ${styles.container_alignments}`}>
+        <div className={styles.section_left}>
+          <img src={BrandLogo} alt="BrandLogo" height={40} />
+          <div className={styles.brand_name}>Config Store</div>
+          <nav className={styles.page_nav}>
+            <div className={styles.page_nav_option}>Home</div>
+            <div className={styles.page_nav_option}>Tickets & Services</div>
+            <div className={styles.page_nav_option}>Notifications</div>
+          </nav>
+        </div>
         <div className={styles.section_right}>right half</div>
       </div>
       <div className={`${styles.container} ${styles.fake_container}`}></div>
